28e2f7d2986dbc0febc9a3fc79006fffca9c1cbe
ted <jans.ted.chen@gmail.com>
2022-06-15 18:01:12 +0800

    Rework

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app" class="flex flex-col items-center">
-    <InfoBlock cName="GeneralInfo"/>
+    <InfoCard :userInfo='userInfo' :submitted='submitted' >GeneralInfo</InfoCard>
   </div>
 </template>
 
 <script>
 // import OuterBox from './components/OuterBox.vue'
-import InfoBlock from './components/InfoBlock.vue'
+import InfoCard from './components/InfoCard.vue'
 
 export default {
   name: 'App',
   components: {
-    InfoBlock
+    InfoCard
+  },
+  data () {
+    return {
+      userInfo: {
+        name: '',
+        email: '',
+        phone: ''
+      },
+      initialized: false,
+      submitted: false
+    }
+  },
+  created () {
+    const appData = !!localStorage.getItem('appData')
+    if (!appData) {
+      localStorage.setItem('appData', JSON.stringify(this.$data))
+    } else {
+      this.submitted = appData.submitted
+    }
   }
 }
 </script>
--- src/components/InfoBlock.vue
+++ src/components/InfoBlock.vue
@@ -1,100 +1,48 @@
 <template>
-  <div class="mt-8 relative w-3/4 border rounded-lg p-2 shadow">
-    <h1 class="text-center text-xl font-semibold mb-4">GeneralInfo</h1>
-    <form class="grid grid-cols-2">
-      <div>
-        <img src="" alt="Portrait"/>
-      </div>
-      <div>
-        <p v-if="isSubmitted" class="text-lg">{{ storedInfo.name }}</p>
-        <InputSet v-if="!isSubmitted" labelId="name" :inputValue="storedInfo.name" :isRequired="true" :inputPattern="valuePattern.plainText" />
-        <p v-if="isSubmitted">{{ storedInfo.email }}</p>
-        <InputSet v-if="!isSubmitted" labelId="email" :inputValue="storedInfo.email" inputType="email"/>
-        <p v-if="isSubmitted">{{ storedInfo.phone }}</p>
-        <InputSet v-if="!isSubmitted" labelId="phone" :inputValue="storedInfo.phone" :inputPattern="valuePattern.numbers" />
-      </div>
-      <button @click.prevent="submitForm" class="absolute top-2 right-4 rounded py-1 px-2 text-sm" :class="buttonClass">{{ formStatus }}</button>
-    </form>
-</div>
+  <div class="flex justify-between mb-1 pr-10">
+    <p v-if="isSubmitted"><slot></slot></p>
+    <label v-else :for="labelId">{{ labelValue }}</label>
+    <input
+      ref="input"
+      class="ml-1 border"
+      :id="labelId"
+      :type="inputType"
+      :pattern="valueFormat"
+      :required="isRequired"
+      :value="inputValue"
+      @blur="checkValidity"
+      @input="checkValidity"
+    />
+  </div>
 </template>
 
 <script>
-import InputSet from './InputSet'
-
+// ^|
 export default {
-  props: ['cName'],
-  name: 'GeneralInfo',
-  components: {
-    InputSet
-  },
+  name: 'InfoBlock',
   data () {
     return {
-      labelMap: {
-        name: 'name',
-        email: 'email',
-        phone: 'phone'
-      },
-      valuePattern: {
-        plainText: {
-          format: "[^-0-9._!&\x22`'#%,:;<>=@{}~$()*+/\\?\\x5B\\x5D^|]+",
-          errMsg: 'Please using words only'
-        },
-        numbers: {
-          format: '[0-9]+',
-          errMsg: 'Please use numbers only'
-        }
-      },
-      isSubmitted: false
+
     }
   },
   computed: {
-    formStatus () {
-      return this.isSubmitted ? 'Edit' : 'Submit'
+    labelValue () {
+      return this.labelId[0].toUpperCase() + this.labelId.slice(1) + ':'
     },
-    buttonClass () {
-      return this.isSubmitted
-        ? 'bg-amber-200 text-amber-800 w-14'
-        : 'bg-green-600 text-green-100'
+    valueFormat () {
+      return this.inputPattern.format
     },
-    storedInfo () {
-      return JSON.parse(localStorage.getItem(this.$options.name))
+    onInvalidMsg () {
+      return this.inputPattern.errMsg
     }
   },
   methods: {
-    submitForm (e) {
-      if (this.isSubmitted === true) {
-        this.isSubmitted = !this.isSubmitted
-        return
-      }
-      const formEl = e.target.closest('form')
-      if (formEl.checkValidity()) {
-        this.isSubmitted = !this.isSubmitted
-        const formData = new FormData(formEl)
-        const userInputs = {}
-        for (const [key, value] of formData.entries(this.$options.name)) {
-          userInputs[key] = value
-        }
-        localStorage.setItem(this.$options.name, JSON.stringify(userInputs))
+    checkValidity () {
+      if (this.$refs.input.validity.patternMismatch) {
+        this.$refs.input.setCustomValidity(this.onInvalidMsg)
       } else {
-        formEl.reportValidity()
+        this.$refs.input.setCustomValidity('')
       }
-    }
-  },
-  mounted () {
-    const storedData = JSON.parse(localStorage.getItem(this.$options.name))
-    console.log(this.labelMap)
-    if (!storedData) {
-      const initValue = {}
-      this.labelMap.keys().forEach(key => {
-        this.initValue[key] = ''
-      })
-      localStorage.setItem(this.$options.name, JSON.stringify(initValue))
-    }
-    const hasSubmitted = !!localStorage.getItem('InitialStatus')
-    if (!hasSubmitted) {
-      localStorage.setItem('InitialStatus', JSON.stringify(this.isSubmitted))
-    } else {
-      this.isSubmitted = hasSubmitted
     }
   }
 }
